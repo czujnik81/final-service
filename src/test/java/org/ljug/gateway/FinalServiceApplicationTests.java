6a88e5e9f3de8faa6658157d269d49aa057536f3
Pawel Janusz <czujnik@gmail.com>
2018-03-23 08:24:56 +0100

    Sleuth dependency M9 upgrade

--- src/test/java/org/ljug/gateway/FinalServiceApplicationTests.java
+++ src/test/java/org/ljug/gateway/FinalServiceApplicationTests.java
@@ -3,10 +3,12 @@ package org.ljug.gateway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = {"ZIPKIN_URL = localhost:9411"})
 public class FinalServiceApplicationTests {
 
 	@Test
